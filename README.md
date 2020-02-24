700bf509ae41d009efb5caf94fba2f1483950928
pulsecode <12067255+dfirsec@users.noreply.github.com>
2020-02-23 21:05:14 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,98 @@
+# IOC Finder
+
+![Generic badge](https://img.shields.io/badge/python-3.7-blue.svg) [![Twitter](https://img.shields.io/badge/Twitter-@pulsecode-blue.svg)](https://twitter.com/pulsecode)
+
+```ioc_finder.py``` Quick and dirty method to search for filenames that match IOCs if file hashes are not yet available.  A better and more comprehensive method would be to use Yara.
+
+```console
+
+          ________  ______   _______           __
+         /  _/ __ \/ ____/  / ____(_)___  ____/ /__  _____
+         / // / / / /      / /_  / / __ \/ __  / _ \/ ___/
+       _/ // /_/ / /___   / __/ / / / / / /_/ /  __/ /
+      /___/\____/\____/  /_/   /_/_/ /_/\__,_/\___/_/
+
+usage: ioc_finder.py [-h] (-i IOC [IOC ...] | -f) path
+
+positional arguments:
+  path                  Path to search
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -i IOC [IOC ...], --ioc IOC [IOC ...]
+                        Single or list of IOCs (comma/space separated)
+  -f, --file            Uses 'known_iocs.txt' file containing IOCs
+```
+
+## Installation
+
+```text
+git clone https://github.com/dfirsec/ioc_finder.git
+cd ioc_finder
+pip install -r requirements.txt
+```
+
+## Options
+
+### -i/--ioc option
+
+```text
+python ioc_finder.py c:\ sysname --ioc bad
+⮩ Searching for IOCs on SYS-NAME: 38934 files [00:08, 4794.81 files/s]
+✔ Found 2 IOCs on SYS-NAME
+    --> Results saved to results\SYS-NAME_20200220-203455.csv
+```
+
+The *--ioc* option uses a wildcard match (\*) for anything after the end of the string, e.g, searching for 'bad' would yield 'badapple', 'badfruit', 'badtaste', etc, and also ignores the string case.
+
+You can also search using a list of items (comma/space separated):
+```python ioc_finder.py c:\ sysname -i bad, pizza, cheese, apple```
+
+Adding a '.' to the end of the string will return the string + any extension.
+
+```text
+python ioc_finder.py c:\ sysname --ioc bad.
++------------------------------------------|+---------+------------+----------------------------------+
+| File name                                |    Size  | Created    | Hash                             |
++------------------------------------------|+---------+------------+----------------------------------+
+| c:\Program Files\Microsoft\bad.exe       |  120214  | 2018-12-21 | 34d70beb5434t4rgfvbd73799b50d125 |
+| c:\Windows\bad.txt                       |     670  | 2019-02-29 | cd5bc2aaed4c6brjyth1eabcf34285de |
+| c:\Windows\bad.lnk                       |     429  | 2018-02-15 | 500758431b795b776e4erdfwed700cef |
++------------------------------------------+----------+------------+----------------------------------+
+```
+
+### -f/--file option
+
+The *--file* option is when you need to search for many filenames. It's currently limited to exact filename matching, however, it's case insensitive (will match upper and lower).
+
+Add your IOC filenames to the *'known_iocs.txt'* text file.
+
+```text
+# ADD IOC FILENAMES BELOW THIS LINE
+badstuff.txt
+badexe.exe
+Xdggrphr.lnk
+lookhere.dll
+```
+
+Example run...
+
+```text
+python ioc_finder.py c:\ sysname -f
+⮩ Searching for IOCs on SYS-NAME: 38934 files [00:08, 4794.81 files/s]
+✔ Found 2 IOCs on SYS-NAME
+    --> Results saved to results\SYS-NAME_20200220-203455.csv
+```
+
+Results are saved to a file and presented with the name, size, creation date, and hash:
+
+```text
++------------------------------------------|+---------+------------+----------------------------------+
+| File name                                |    Size  | Created    | Hash                             |
++------------------------------------------|+---------+------------+----------------------------------+
+| c:\Program Files\Microsoft\badexe.exe    |  140288  | 2018-12-20 | 34d70beb5434c95bd73799b55ea0d125 |
+| c:\Windows\badstuff.txt                  |     470  | 2019-08-23 | cd5bc2aaed4c6b8a21eabcf34285d69e |
+| c:\Windows\Xdggrphr.lnk                  |     462  | 2019-08-23 | 5007584931b795b776e6b15f4d700cef |
+| c:\Windows\lookhere.dll                  |    6836  | 2019-08-23 | 68baa20566a1afa2319e6afc5942e056 |
++------------------------------------------+----------+------------+----------------------------------+
+```
---
+++ ioc_finder.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python
+
+__author__ = "DFIRSec (@pulsecode)"
+__description__ = "Quick and dirty method to search for filenames that match IOCs if hashes are not yet available."
+
+# TODO: Consolidate and streamline functions
+
+import csv
+import hashlib
+import os
+import socket
+import sys
+import time
+from argparse import ArgumentParser
+from datetime import datetime
+from pathlib import Path
+
+from colorama import Back, Fore, Style, init
+from tqdm import tqdm
+from wcmatch import fnmatch
+
+
+class Workers(object):
+    def __init__(self, count=None):
+        self.count = count
+
+    filepath = Path(__file__).parent
+    results = filepath / 'results'
+    iocs = filepath / 'iocs'
+    hostname = socket.gethostname().upper()
+
+    # Unicode Symbols and colors -  ref: http://www.fileformat.info/info/unicode/char/a.htm
+    processing = Fore.CYAN + "\u2BA9" + Style.RESET_ALL     # ⮩
+    found = Fore.GREEN + "\u2714" + Style.RESET_ALL         # ✔
+    notfound = Fore.YELLOW + "\u00D8" + Style.RESET_ALL     # Ø
+    error = Fore.RED + "\u2718" + Style.RESET_ALL           # ✘
+
+    def iocs_file(self):
+        return self.iocs / 'known_iocs.txt'
+
+    def save_iocs_csv(self):
+        if not self.results.exists():
+            self.results.mkdir(parents=True)
+        timestr = time.strftime("%Y%m%d-%H%M%S")
+        return self.results / f'{WRK.hostname}_{timestr}.csv'
+
+    def sha256(self, fname):
+        hash_sha256 = hashlib.sha256()
+        with open(fname, "rb") as f:
+            for chunk in iter(lambda: f.read(4096), b""):
+                hash_sha256.update(chunk)
+        return hash_sha256.hexdigest()
+
+    def read_file(self):
+        with open(self.iocs_file(), 'r') as f:
+            next(f)  # skip file header starting with '#'
+            data = [data.strip() for data in f.readlines()]
+        return data
+
+
+def main(drivepath, ioc=None, file=None):
+    # Check if python version is v3.6+
+    if sys.version_info[0] == 3 and sys.version_info[1] <= 5:
+        sys.exit(f"\n{WRK.error} Please use python version 3.6 or higher.\n")
+
+    WRK.count = 0
+    if ioc:
+        with open(WRK.save_iocs_csv(), 'w', newline='') as csvfile:
+            fieldnames = ['Path', 'Size', 'Created', 'Hash']  # nopep8
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)  # nopep8
+            writer.writeheader()
+            for root, _, files in tqdm(os.walk(drivepath),
+                                    ascii=True,
+                                    desc=f"{WRK.processing} Searching for IOCs on {WRK.hostname}",
+                                    ncols=80, unit=" files"):
+                for filename in files:
+                    for item in ioc:
+                        if fnmatch.fnmatch(filename, item+r'*', flags=fnmatch.IGNORECASE):
+                            try:
+                                path = os.path.join(root, filename)
+                                created = datetime.fromtimestamp(os.stat(path).st_ctime)  # nopep8
+                                size = os.stat(path).st_size
+                                writer.writerows([{'Path': path,
+                                                'Size': size,
+                                                'Created': f"{created:%Y-%m-%d}",
+                                                'Hash': WRK.sha256(path)}])
+                                WRK.count += 1
+                            except PermissionError:
+                                continue
+                            except WindowsError:
+                                continue
+                            except Exception as err:
+                                print(f"{WRK.error} {err}")
+
+    elif file:
+        # check if IOC's file is empty (no IOCs)
+        if os.path.getsize(WRK.iocs_file()) < 40:
+            sys.exit(f"\n{WRK.error} Missing IOCs -- The {WRK.iocs_file()} file appears to be empty.\n")  # nopep8
+
+        ioc_str = WRK.read_file()
+        with open(WRK.save_iocs_csv(), 'w', newline='') as csvfile:
+            fieldnames = ['Path', 'Size', 'Created', 'Hash']  # nopep8
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)  # nopep8
+            writer.writeheader()
+            for root, dirs, files in tqdm(os.walk(drivepath),
+                                        ascii=True,
+                                        desc=f"{WRK.processing} Searching for IOCs on {WRK.hostname}",
+                                        ncols=80, unit=" files"):
+                    for filename in files:
+                        if filename.lower() in (name.lower() for name in ioc_str):
+                            try:
+                                path = os.path.join(root, filename)
+                                created = datetime.fromtimestamp(os.stat(path).st_ctime)  # nopep8
+                                size = os.stat(path).st_size
+                                WRK.count += 1
+                                writer.writerows([{'Path': path,
+                                                'Size': size,
+                                                'Created': f"{created:%Y-%m-%d}",
+                                                'Hash': WRK.sha256(path)}])
+                            except (PermissionError, WindowsError):
+                                continue
+                            except Exception as err:
+                                print(f"{WRK.error} {err}")
+            
+    if WRK.count:
+        print(f"\n{WRK.found} Found {WRK.count} IOCs on {WRK.hostname}")
+        print(f"    --> Results saved to {WRK.save_iocs_csv()}")
+    else:
+        print(f"{WRK.notfound} No matches for IOCs")
+        # Remove empty results - not the best method, but it works
+        p = WRK.results.glob('**/*')
+        files = [x for x in p if x.is_file()]
+        for _file in files:
+            if os.stat(_file).st_size < 25:
+                os.remove(_file)
+
+
+if __name__ == '__main__':
+    banner = '''
+          ________  ______   _______           __
+         /  _/ __ \/ ____/  / ____(_)___  ____/ /__  _____
+         / // / / / /      / /_  / / __ \/ __  / _ \/ ___/
+       _/ // /_/ / /___   / __/ / / / / / /_/ /  __/ /
+      /___/\____/\____/  /_/   /_/_/ /_/\__,_/\___/_/
+    '''
+
+    print(Fore.CYAN + banner + Style.RESET_ALL)
+
+    WRK = Workers()
+    parser = ArgumentParser()
+    parser.add_argument('path', help="Path to search")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-i', '--ioc', nargs='+', type=str,
+                       help="Single or list of IOCs (comma/space separated)")
+    group.add_argument('-f', '--file', action='store_true', default=WRK.iocs_file(),
+                       help="Uses 'known_iocs.txt' file containing IOCs")
+
+    args = parser.parse_args()
+
+    if len(sys.argv[1:]) == 0:
+        parser.print_help()
+        parser.exit()
+
+    main(args.path, args.ioc, args.file)
