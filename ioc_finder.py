add392a6e8d07b4d8a7fa7c5388f8e3541d275c9
dfirsec <12067255+dfirsec@users.noreply.github.com>
2021-02-27 08:49:24 -0500

    Minor Update

--- ioc_finder.py
+++ ioc_finder.py
@@ -11,11 +11,11 @@ from pathlib import Path, PurePath
 import requests
 from colorama import Fore
 from colorama import init as color_init
-import prettytable
+from prettytable import PrettyTable
 from tqdm import tqdm
 
 __author__ = "DFIRSec (@pulsecode)"
-__version__ = "v0.0.8"
+__version__ = "v0.0.9"
 __description__ = "Quick and dirty method to search for filenames that match IOCs if hashes are not yet available."
 
 
@@ -24,25 +24,27 @@ class Workers(object):
         self.count = count
 
     filepath = Path(__file__).parent
-    results = filepath / 'results'
-    iocs = filepath / 'iocs'
+    results = filepath / "results"
+    iocs = filepath / "iocs"
     hostname = socket.gethostname().upper()
+
+    # Initialize colorama
     color_init()
 
     # Unicode Symbols and colors -  ref: http://www.fileformat.info/info/unicode/char/a.htm
-    processing = f'{Fore.CYAN}\u2BA9{Fore.RESET}'
-    found = f'{Fore.GREEN}\u2714{Fore.RESET}'
-    notfound = f'{Fore.YELLOW}\u00D8{Fore.RESET}'
-    error = f'{Fore.RED}\u2718{Fore.RESET}'
+    processing = f"{Fore.CYAN}>{Fore.RESET}"
+    found = f"{Fore.GREEN}\u2714{Fore.RESET}"
+    notfound = f"{Fore.YELLOW}\u00D8{Fore.RESET}"
+    error = f"{Fore.RED}\u2718{Fore.RESET}"
 
     def iocs_file(self):
-        return self.iocs / 'known_iocs.txt'
+        return self.iocs / "known_iocs.txt"
 
     def save_iocs_csv(self):
         if not self.results.exists():
             self.results.mkdir(parents=True)
         timestr = time.strftime("%Y%m%d-%H%M%S")
-        return self.results / f'{WRK.hostname}_{timestr}.csv'
+        return self.results / f"{WRK.hostname}_{timestr}.csv"
 
     @staticmethod
     def sha256(fname):
@@ -53,26 +55,31 @@ class Workers(object):
         return hash_sha256.hexdigest()
 
     def read_file(self):
-        with open(self.iocs_file(), 'r') as f:
-            next(f)  # skip file header starting with '#'
+        with open(self.iocs_file(), "r") as f:
+            # skip file header starting with '#'
+            next(f)
             data = [data.strip() for data in f.readlines()]
         return data
 
 
 def ptable_to_term():
     # output latest csv file to terminal
-    csv_files = Path(WRK.results).glob('*.csv')
+    csv_files = Path(WRK.results).glob("*.csv")
     latest_csv = max(csv_files, key=os.path.getctime)
 
-    with open(latest_csv) as f:
-        x = prettytable.from_csv(f, delimiter=',')
-    x.align = 'l'
-    print(x)
+    with open(latest_csv) as fd:
+        rd = csv.reader(fd, delimiter=",")
+        pt = PrettyTable(next(rd))
+        for row in rd:
+            pt.add_row(row)
+
+    pt.align = "l"
+    print(pt)
 
 
 def remove_output():
     # Remove empty results - not the best method, but it works
-    csv_files = Path(WRK.results).glob('*.csv')
+    csv_files = Path(WRK.results).glob("*.csv")
     files = [x for x in csv_files if x.is_file()]
     for csv in files:
         if os.stat(csv).st_size < 25:
@@ -86,29 +93,38 @@ def main(drivepath, ioc=None, infile=None):
 
     WRK.count = 0
     if ioc:
-        # check if ioc contains spaces
-        if [i for i in ioc[:-1] if ',' not in str(i.split(','))]:
-            sys.exit(f'Surround IOC string that contains spaces with double quotes, e.g., {Fore.LIGHTMAGENTA_EX}"find me now.zip"{Fore.RESET}.')  # nopep8
-        with open(WRK.save_iocs_csv(), 'w', newline='') as csvfile:
-            fieldnames = ['Path', 'Size', 'Created', 'Hash']  # nopep8
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)  # nopep8
+        # Check if ioc contains spaces
+        if [i for i in ioc[:-1] if "," not in str(i.split(","))]:
+            sys.exit(f'Surround string with double quotes, e.g., {Fore.LIGHTMAGENTA_EX}"find me now.zip"{Fore.RESET}.')
+        with open(WRK.save_iocs_csv(), "w", newline="") as csvfile:
+            fieldnames = ["Path", "Size", "Created", "Hash"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
             try:
-                for root, _, files in tqdm(os.walk(drivepath),
-                                           ascii=True,
-                                           desc=f"{WRK.processing} Searching for IOCs on {WRK.hostname}",
-                                           ncols=80, unit=" files"):
+                for root, _, files in tqdm(
+                    os.walk(drivepath),
+                    ascii=True,
+                    desc=f"{WRK.processing} Searching for IOCs on {WRK.hostname}",
+                    ncols=80,
+                    unit=" files",
+                ):
                     for filename in files:
                         for item in ioc:
                             try:
-                                if PurePath(filename).match(item.strip(',')+r'*'):
+                                if PurePath(filename).match(item.strip(",") + r"*"):
                                     path = os.path.join(root, filename)
-                                    created = datetime.fromtimestamp(os.stat(path).st_ctime)  # nopep8
+                                    created = datetime.fromtimestamp(os.stat(path).st_ctime)
                                     size = os.stat(path).st_size
-                                    writer.writerows([{'Path': path,
-                                                       'Size': size,
-                                                       'Created': f"{created:%Y-%m-%d}",
-                                                       'Hash': WRK.sha256(path)}])
+                                    writer.writerows(
+                                        [
+                                            {
+                                                "Path": path,
+                                                "Size": size,
+                                                "Created": f"{created:%Y-%m-%d}",
+                                                "Hash": WRK.sha256(path),
+                                            }
+                                        ]
+                                    )
                                     WRK.count += 1
                             except (PermissionError, WindowsError):
                                 continue
@@ -120,31 +136,40 @@ def main(drivepath, ioc=None, infile=None):
                 sys.exit("\nAborted!")
 
     elif infile:
-        # check if IOC's file is empty (no IOCs)
+        # Check if IOC's file is empty (no IOCs)
         if os.path.getsize(WRK.iocs_file()) < 40:
-            sys.exit(f"\n{WRK.error} Missing IOCs -- The {WRK.iocs_file()} file appears to be empty.\n")  # nopep8
+            sys.exit(f"\n{WRK.error} Missing IOCs -- The {WRK.iocs_file()} file appears to be empty.\n")
 
         ioc_str = WRK.read_file()
-        with open(WRK.save_iocs_csv(), 'w', newline='') as csvfile:
-            fieldnames = ['Path', 'Size', 'Created', 'Hash']  # nopep8
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)  # nopep8
+        with open(WRK.save_iocs_csv(), "w", newline="") as csvfile:
+            fieldnames = ["Path", "Size", "Created", "Hash"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
             try:
-                for root, _, files in tqdm(os.walk(drivepath),
-                                           ascii=True,
-                                           desc=f"{WRK.processing} Searching for IOCs on {WRK.hostname}",
-                                           ncols=80, unit=" files"):
+                for root, _, files in tqdm(
+                    os.walk(drivepath),
+                    ascii=True,
+                    desc=f"{WRK.processing} Searching for IOCs on {WRK.hostname}",
+                    ncols=80,
+                    unit=" files",
+                ):
                     for filename in files:
                         if filename.lower() in (name.lower() for name in ioc_str):
                             try:
                                 path = os.path.join(root, filename)
-                                created = datetime.fromtimestamp(os.stat(path).st_ctime)  # nopep8
+                                created = datetime.fromtimestamp(os.stat(path).st_ctime)
                                 size = os.stat(path).st_size
                                 WRK.count += 1
-                                writer.writerows([{'Path': path,
-                                                   'Size': size,
-                                                   'Created': f"{created:%Y-%m-%d}",
-                                                   'Hash': WRK.sha256(path)}])
+                                writer.writerows(
+                                    [
+                                        {
+                                            "Path": path,
+                                            "Size": size,
+                                            "Created": f"{created:%Y-%m-%d}",
+                                            "Hash": WRK.sha256(path),
+                                        }
+                                    ]
+                                )
                             except (PermissionError, WindowsError):
                                 continue
                             except Exception as err:
@@ -163,8 +188,8 @@ def main(drivepath, ioc=None, infile=None):
         remove_output()
 
 
-if __name__ == '__main__':
-    banner = fr'''
+if __name__ == "__main__":
+    banner = fr"""
           ________  ______   _______           __
          /  _/ __ \/ ____/  / ____(_)___  ____/ /__  _____
          / // / / / /      / /_  / / __ \/ __  / _ \/ ___/
@@ -173,26 +198,26 @@ if __name__ == '__main__':
       
                                     {__version__}
                                     {__author__}
-    '''
+    """
 
     print(f"{Fore.CYAN}{banner}{Fore.RESET}")
 
     WRK = Workers()
     parser = ArgumentParser()
-    parser.add_argument('path', help="Path to search")
+    parser.add_argument("path", help="Path to search")
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('-i', nargs='+', type=str, metavar='',
-                       help="Single or list of IOCs (comma separated)")
-    group.add_argument('-f', action='store_true', default=WRK.iocs_file(),
-                       help="Uses 'known_iocs.txt' file containing IOCs")
+    group.add_argument("-i", nargs="+", type=str, metavar="", help="Single or list of IOCs (comma separated)")
+    group.add_argument(
+        "-f", action="store_true", default=WRK.iocs_file(), help="Uses 'known_iocs.txt' file containing IOCs"
+    )
 
     args = parser.parse_args()
 
-    # check if new version is available
+    # Check if new version is available
     try:
-        latest = requests.get("https://api.github.com/repos/dfirsec/ioc_finder/releases/latest").json()["tag_name"]  # nopep8
+        latest = requests.get("https://api.github.com/repos/dfirsec/ioc_finder/releases/latest").json()["tag_name"]
         if latest != __version__:
-            print(f"{Fore.YELLOW}* Release {latest} of ioc_finder is available{Fore.RESET}")  # nopep8
+            print(f"{Fore.YELLOW}* Release {latest} of ioc_finder is available{Fore.RESET}")
     except Exception as err:
         print(f"{Fore.LIGHTRED_EX}[Error]{Fore.RESET} {err}\n")
 
